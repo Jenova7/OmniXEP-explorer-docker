89f98a2a9925512339bacb9c7a4ce64ae5bc1209
MotoAcidic <nexusnow@protonmail.com>
2023-11-13 10:54:38 -0500

    Changes from original author

--- engine/run.sh
+++ engine/run.sh
@@ -5,19 +5,18 @@
 # echo "Environment variables OMNIDB_DATABASE must be set"
 # exit 1
 #fi
-#OPTIONS="--echo-all -v ON_ERROR_STOP=1"
-#echo "Create Database..."
-#if psql -lqt | cut -d \| -f 1 | grep -qw $OMNIDB_DATABASE; then
-#    echo "Database exists"
+OPTIONS="--echo-all -v ON_ERROR_STOP=1"
+echo "Create Database..."
+if psql -lqt | cut -d \| -f 1 | grep -qw $OMNIDB_DATABASE; then
+    echo "Database exists"
     # database exists
     # $? is 0
-#else
-#    createdb "${OMNIDB_DATABASE}" "Omniwallet wallet and transaction database"
-#fi
+else
+    createdb "${OMNIDB_DATABASE}" "Omniwallet wallet and transaction database"
+fi
 
-#cd /flyway && FLYWAY_URL="jdbc:postgresql://${PGHOST}:${PGPORT}/${OMNIDB_DATABASE}" FLYWAY_PASSWORD="${PGPASSWORD}" flyway -user=${PGUSER} migrate
-/home/ubuntu/omniEngine/database/scripts/db-init.sh
+cd /flyway && FLYWAY_URL="jdbc:postgresql://${PGHOST}:${PGPORT}/${OMNIDB_DATABASE}" FLYWAY_PASSWORD="${PGPASSWORD}" flyway -user=${PGUSER} migrate
 
-python /home/ubuntu/omniEngine/install/installOmniEngineCronJob.py
+python /root/omniEngine/install/installOmniEngineCronJob.py
 
 /usr/sbin/cron -f -L /dev/stdout
